1c6a7c0b28456e445bf11ef41c1c6cc10995a45c
Natalie Davidovic <natalija.david@hotmail.com>
2021-02-16 19:41:21 -0800

    Add all appointment views

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -157,4 +158,9 @@ storiesOf("Appointment", module)
   .add("Status", () =>
     <Status
       message={"deleting"}
+    />)
+  .add("Error", () =>
+    <Error
+      message={"Could not delete appointment."}
+      onClose={action("onClose")}
     />);
