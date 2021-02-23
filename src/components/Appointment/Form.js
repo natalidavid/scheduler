e330dfa577fe993074ba81f48db423e4929bbcd8
Natalie Davidovic <natalija.david@hotmail.com>
2021-02-22 20:48:54 -0800

    Refactor spots; add form tests

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,7 +3,6 @@ import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
-
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   const [error, setError] = useState("");
@@ -23,26 +22,23 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    setError("");
     props.onSave(name, interviewer);
   }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form
-          autoComplete="off"
-          onSubmit={event => event.preventDefault()}>
-
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
             value={name}
-            onChange={event => setName(event.target.value)}
+            onChange={(event) => setName(event.target.value)}
             data-testid="student-name-input"
           />
-          
         </form>
         <section className="appointment__validation">{error}</section>
         <InterviewerList
@@ -53,14 +49,15 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button
-            danger
-            onClick={cancel}>Cancel</Button>
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
           <Button
             confirm
             // onClick={() => props.onSave(name, interviewer)}
             onClick={validate}
-            >Save
+          >
+            Save
           </Button>
         </section>
       </section>
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,85 +1,104 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 export default function useApplicationData(props) {
-
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {},
-    interviewers: {}
+    interviewers: {},
   });
 
-  const setDay = day => setState({ ...state, day });
+  const setDay = (day) => setState({ ...state, day });
 
-  const updateState = function () {
+  // document ready
+  useEffect(() => {
     const urlDays = `/api/days`;
     const urlAppointments = `/api/appointments`;
     const urlInterviewers = `/api/interviewers`;
 
-    return Promise.all([
+    Promise.all([
       axios.get(urlDays),
       axios.get(urlAppointments),
       axios.get(urlInterviewers)
-    ])
-      .then((all) => {
-        const days = all[0].data;
-        const appointments = all[1].data;
-        const interviewers = all[2].data;
-        console.log("all", all);
 
-        setState(prev => ({ ...prev, days, appointments, interviewers }));
-      });
+    ]).then((all) => {
+      const days = all[0].data;
+      const appointments = all[1].data;
+      const interviewers = all[2].data;
+      console.log("all", all);
+
+      setState(prev => ({ ...prev, days, appointments, interviewers }));
+    });
+  }, []);
+
+
+  //find only returns one thing
+  const getSpotsForDay = function (day, appointments) {
+    let spots = 0;
+    for (const id of day.appointments) {
+      const appointment = appointments[id];
+      if (!appointment.interview) {
+        spots++;
+      }
+    }
+    return spots;
   };
 
-  // document ready
-  useEffect(() => {
-    updateState();
-  }, []);
+  const updateSpots = function (dayName, days, appointments) {
+    //find the day object
+    const day = days.find((d) => d.name === dayName);
+    const spots = getSpotsForDay(day, appointments);
+    return days.map((item) =>
+      item.name === dayName ? { ...day, spots } : item
+    );
+  };
 
   // Interviews: Create, Delete, Edit:
   function bookInterview(id, interview) {
-
     const appointment = {
       ...state.appointments[id],
-      interview: { ...interview }
+      interview: { ...interview },
     };
 
     const appointments = {
       ...state.appointments,
-      [id]: appointment
+      [id]: appointment,
     };
 
     const url = `/api/appointments/${id}`;
     return axios.put(url, { interview })
       .then(() => {
-        setState({ ...state, appointments });
-      })
-      .then(() => { return updateState(); })
-      .catch(error => console.log(error));
-  }
+        const days = updateSpots(state.day, state.days, appointments)
+        setState({ ...state, appointments, days });
+      });
+
+  };
 
   // deleting interviews
   function cancelInterview(id, interview) {
-
     const appointment = {
       ...state.appointments[id],
-      interview: null
+      interview: null,
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
     };
 
     const url = `/api/appointments/${id}`;
-    return axios.delete(url, { interview })
-      .then(() => {
-        setState({ ...state, appointment });
-      })
-      .then(() => { return updateState(); })
-      .catch(error => console.log(error));
+    return axios.delete(url)
+    .then(() => {
+      const days = updateSpots(state.day, state.days, appointments)
+      setState({ ...state, appointments, days });
+    });
   }
 
   return {
     state,
     setDay,
     bookInterview,
-    cancelInterview
+    cancelInterview,
   };
 }
